88c805a08c3c35f2f37b065390bd63b82ecb6aed
Nguyen Truong Vu <913.nguyentruongvu2018@gmail.com>
2025-12-18 19:46:59 +0700

    Fix CORS error

--- backend/api.php
+++ backend/api.php
@@ -1,11 +1,15 @@
 <?php
-// Mở cửa cho Vercel vào (CORS)
+// Chỉ cần giữ lại một bộ header chuẩn này là đủ để sửa lỗi CORS
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { http_response_code(200); exit(); }
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { 
+    http_response_code(200); 
+    exit(); 
+}
+
 
 // --- THAY THÔNG TIN CỦA BẠN VÀO ĐÂY ---
 $host = "sql100.infinityfree.com"; // Thay Host Name
